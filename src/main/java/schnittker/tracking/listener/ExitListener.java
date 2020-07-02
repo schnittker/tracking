40ba3194f34caa4bfc1152b0d5f14c2f78653b36
Markus Schnittker <schnittker@neozo.de>
2020-07-02 18:53:49 +0200

    add exit dialog

--- src/main/java/schnittker/tracking/listener/ExitListener.java
+++ src/main/java/schnittker/tracking/listener/ExitListener.java
@@ -1,11 +1,25 @@
 package schnittker.tracking.listener;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class ExitListener implements ActionListener {
+    private final ResourceBundle translations;
+
+    public ExitListener() {
+        translations = ResourceBundle.getBundle("i18n.Messages", Locale.getDefault());
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.exit(0);
+        int dialogResult = JOptionPane.showConfirmDialog (null, translations.getString("dialog.exit.message"),
+                translations.getString("dialog.exit.headline"), JOptionPane.YES_NO_OPTION);
+
+        if(dialogResult == JOptionPane.YES_OPTION){
+            System.exit(0);
+        }
     }
 }
